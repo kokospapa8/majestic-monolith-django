823c27a6428997c24ff890537f8e2481e9579fe8
kokospapa <kokos.papa8@gmail.com>
2022-04-16 19:32:08 +0900

    cookie cutter init

--- majestic-monolith-django/shipping/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from django.contrib import admin
-
-
-class ShippingConfig(AppConfig):
-    name = "shipping"
-    label = "shipping"
-    verbose_name = 'shipping'
